5e83c97734f07ad084ca3b53886d7d59ded75d29
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-06-26 21:53:24 -0400

    Intregated sqlite to the rest of my pages just in case

--- Bus Seater/Login.swift
+++ Bus Seater/Login.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SQLite3
 
 struct Login: View {
     var body: some View {
--- Bus Seater/SplashScreen.swift
+++ Bus Seater/SplashScreen.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SQLite3
 
 struct SplashScreen: View {
     var body: some View {
