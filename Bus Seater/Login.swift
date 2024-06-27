31c5e01cb0b40527bbf0b22ac8455d02e071f736
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-06-27 17:21:22 -0400

    Added text fields to login page

--- Bus Seater/Login.swift
+++ Bus Seater/Login.swift
@@ -9,17 +9,23 @@ import SwiftUI
 import SQLite3
 
 struct Login: View {
+    @State var username: String = ""
+    @State var password: String  = ""
     var body: some View {
         ZStack {
             Color(.white)
                 .ignoresSafeArea()
             
-            VStack {
+            VStack(spacing: 50) {
                 Text("Welcome to Bus Seater, the world's first bus seating app!")
                     .multilineTextAlignment(.center)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                TextField("Username", text: $username)
+                TextField("Password", text: $password)
                 Spacer()
                     .frame(height: 250)
+
+                
             }
             .padding()
         }
