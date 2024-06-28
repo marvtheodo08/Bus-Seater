dc06deb5cb393de5732e010acdd668ffb923081d
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-06-28 15:29:42 -0400

    I further edited the login screen by adding textfields and a button for account sign ups

--- Bus Seater/Login.swift
+++ Bus Seater/Login.swift
@@ -16,24 +16,25 @@ struct Login: View {
             Color(.white)
                 .ignoresSafeArea()
             
-            VStack(spacing: 50) {
+            VStack {
                 Text("Welcome to Bus Seater, the world's first bus seating app!")
                     .multilineTextAlignment(.center)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                TextField("Username", text: $username)
-                TextField("Password", text: $password)
-                Text("Don't have an account? Sign up here!")
-                    .foregroundStyle(.blue)
-                
                 Spacer()
-                    .frame(height: 10)
-                
+                    .frame(height: 50)
+                TextField("Username", text: $username)
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
+                TextField("Password", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
+                Button(action: {}, label: {Text("Don't have an account? Sign up here!")})
             }
             .padding()
         }
     }
 }
-
+        
 #Preview {
     Login()
 }
