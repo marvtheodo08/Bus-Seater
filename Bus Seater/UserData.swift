6ef90f30c293c5338498f2fb4fd3049bb551ece6
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-07-02 17:20:19 -0400

    Update UserData.swift
    I've removed the parent class and all mentions of it from the data table. This is because having parents oversee where their child sits and whether they approve of it sounds a bit extreme.

--- Bus Seater/UserData.swift
+++ Bus Seater/UserData.swift
@@ -13,19 +13,17 @@ import SwiftData
 class student{
     var username: String?
     var password: String?
-    var First: String?
+    var first: String?
     var last: String?
-    @Relationship var parents: [parent] = []
     @Relationship var school: String?
     var grade: String?
     var grade_abriv: String?
     @Relationship var bus: bus?
-    init(username: String? = nil, password: String? = nil, First: String? = nil, last: String? = nil, parents: [parent] = [], school: String? = nil, grade: String? = nil, grade_abriv: String? = nil, bus: bus? = nil) {
+    init(username: String? = nil, password: String? = nil, first: String? = nil, last: String? = nil, school: String? = nil, grade: String? = nil, grade_abriv: String? = nil, bus: bus? = nil) {
         self.username = username
         self.password = password
-        self.First = First
+        self.first = first
         self.last = last
-        self.parents = parents
         self.school = school
         self.grade = grade
         self.grade_abriv = grade_abriv
@@ -34,34 +32,17 @@ class student{
 }
 
 @Model
-class parent{
-    var username: String?
-    var password: String?
-    var First: String?
-    var last: String?
-    @Relationship var offspring: [student] = []
-    init(username: String? = nil, password: String? = nil, First: String? = nil, last: String? = nil, offspring: [student] = []){
-        self.username = username
-        self.password = password
-        self.First = First
-        self.last = last
-        self.offspring = offspring
-    }
-    
-}
-
-@Model
 class driver{
     var username: String?
     var password: String?
-    var First: String?
+    var first: String?
     var last: String?
     @Relationship var school: school?
     @Relationship var bus: bus?
-    init(username: String? = nil, password: String? = nil, First: String? = nil, last: String? = nil, school: school? = nil, bus: bus? = nil) {
+    init(username: String? = nil, password: String? = nil, first: String? = nil, last: String? = nil, school: school? = nil, bus: bus? = nil) {
         self.username = username
         self.password = password
-        self.First = First
+        self.first = first
         self.last = last
         self.school = school
         self.bus = bus
