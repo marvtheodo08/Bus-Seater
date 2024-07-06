1492770183f0294536045205d302a9bad995dd8d
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-07-06 18:37:31 -0400

    I've moved the addSchools function from the Context View file to the main application file.

--- Bus Seater/Bus_SeaterApp.swift
+++ Bus Seater/Bus_SeaterApp.swift
@@ -9,13 +9,30 @@ import SwiftUI
 import SwiftData
 
 
-
 @main
 struct Bus_SeaterApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear{
+                    if !UserDefaults.standard.bool(forKey: "preDataInserted") {
+                        addSchools()
+                        UserDefaults.standard.set(true, forKey: "preDataInserted")
+                    }
+                }
         }
         .modelContainer(for: [student.self, driver.self, school.self, bus.self])
     }
 }
+
+func addSchools()
+{
+    @Environment(\.modelContext) var modelContext
+    let schools =
+    [school(school_name: "South Shore Charter Public School", municipality: "Norwell", state: "MA")]
+    for school in schools {
+        modelContext.insert(school)
+    }
+}
+
+
