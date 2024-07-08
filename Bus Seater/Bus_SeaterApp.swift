74f36a88c516b807bcaa7581e66d19102a5932b8
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-07-08 00:03:28 -0400

    I'v yet again move the addScools function back to the Content View file, and I've moved the main route of my app to a Group Bracket. When the Group view appears for the very first time, it will set the UserDefault bool of "SchoolDataInserted" as false, and it will perform addSchools and make it true. Because the bool is already set to true in subsequent launches of the app, it will prevent data duplication.

--- Bus Seater/Bus_SeaterApp.swift
+++ Bus Seater/Bus_SeaterApp.swift
@@ -11,28 +11,12 @@ import SwiftData
 
 @main
 struct Bus_SeaterApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .onAppear{
-                    if !UserDefaults.standard.bool(forKey: "SchoolDataInserted") {
-                        addSchools()
-                        UserDefaults.standard.set(true, forKey: "SchoolDataInserted")
-                    }
-                }
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
+            .modelContainer(for: [student.self, driver.self, school.self, bus.self])
         }
-        .modelContainer(for: [student.self, driver.self, school.self, bus.self])
-    }
-}
 
-func addSchools()
-{
-    @Environment(\.modelContext) var modelContext
-    let schools =
-    [school(school_name: "South Shore Charter Public School", municipality: "Norwell", state: "MA")]
-    for school in schools {
-        modelContext.insert(school)
     }
-}
-
 
--- Bus Seater/ContentView.swift
+++ Bus Seater/ContentView.swift
@@ -8,33 +8,54 @@
 import SwiftUI
 import SwiftData
 
-
-
-
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
+    @Environment(\.modelContext) var modelContext
     @State private var isSplash = true
     var body: some View{
-        if isSplash{
-            SplashScreen()
-                .onAppear{
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2){
-                        withAnimation(.easeOut(duration: 0.5))
-                        {
-                            isSplash = false
+        Group{
+            if isSplash{
+                SplashScreen()
+                    .onAppear{
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                            withAnimation(.easeOut(duration: 0.5))
+                            {
+                                isSplash = false
+                            }
+                            
                         }
-                        
                     }
-                }
-            
+                
+            }
+            else
+            {
+                Login()
+            }
         }
-        else
-        {
-            Login()
+        .onAppear {
+           if !UserDefaults.standard.bool(forKey: "SchoolDataInserted") {
+               addSchools()
+               UserDefaults.standard.set(true, forKey: "SchoolDataInserted")
+               
+           }
+       }
+
+    }
+    func addSchools() {
+        let schools = [
+            school(school_name: "South Shore Charter Public School", municipality: "Norwell", state: "MA"), school(school_name: "Brockton High School", municipality: "Brockton", state: "MA")]
+        
+        for school in schools {
+            modelContext.insert(school)
+        }
+        
+        do {
+            try modelContext.save()
+        } catch {
+            print("Failed to save context: \(error)")
         }
     }
-
 }
+
 #Preview {
     ContentView()
 }
