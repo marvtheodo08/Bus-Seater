de19265c5580b09cc907621c39bf35c14a3b0c95
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-07-25 00:47:12 -0400

    Update Bus_SeaterApp.swift
    Integrated Log In check with app

--- Bus Seater/Bus_SeaterApp.swift
+++ Bus Seater/Bus_SeaterApp.swift
@@ -11,10 +11,12 @@ import SwiftData
 
 @main
 struct Bus_SeaterApp: App {
+@StateObject private var userSettings = UserSettings()
         var body: some Scene {
             WindowGroup {
                 ContentView()
             }
+            .environmentObject(userSettings)
             .modelContainer(for: [student.self, driver.self, school.self, bus.self, admin.self])
         }
 
