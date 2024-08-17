bac9f63d26000cf0ce101139cbabebefb0ccebbc
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-08-17 07:54:24 -0400

    Integrated Firebase to project

--- Bus Seater/Bus_SeaterApp.swift
+++ Bus Seater/Bus_SeaterApp.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
-struct Bus_SeaterApp: App {
-        var body: some Scene {
-            WindowGroup {
-                ContentView()
-            }
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
+}
 
