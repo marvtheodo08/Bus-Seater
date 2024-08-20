bee038c82f594b1cf7ea06f60756f70779578a27
marvtheodo08 <marvheen.theodore08@gmail.com>
2024-08-20 09:39:47 -0400

    Added necessary code to UserDataManager file

--- Bus Seater /UserDataManager.swift
+++ Bus Seater /UserDataManager.swift
@@ -6,4 +6,42 @@
 //
 
 import Foundation
-import SQLite3
+import SQLite
+
+class DatabaseManager: ObservableObject {
+    private var db: Connection?
+
+    init() {
+        copyDatabaseIfNeeded()
+        connectToDatabase()
+    }
+
+    private func copyDatabaseIfNeeded() {
+        let fileManager = FileManager.default
+        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let databaseURL = documentDirectory.appendingPathComponent("userdata.db")
+
+        if !fileManager.fileExists(atPath: databaseURL.path) {
+            if let bundleURL = Bundle.main.url(forResource: "userdata", withExtension: "db") {
+                do {
+                    try fileManager.copyItem(at: bundleURL, to: databaseURL)
+                } catch {
+                    print("Error copying database file: \(error)")
+                }
+            }
+        }
+    }
+
+    private func connectToDatabase() {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let databaseURL = documentDirectory.appendingPathComponent("userdata.db")
+
+        do {
+            db = try Connection(databaseURL.path)
+        } catch {
+            print("Error connecting to database: \(error)")
+        }
+    }
+
+}
+
