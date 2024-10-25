9186c63c4ff127eab16f43390f9760bb6574b8cd
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-10-25 23:24:54 +0000

    Revert "Delete Bus Seater /NotifsPermission.swift"
    This reverts commit c2f2fa3599520a6ef6db2b0c39391e565cf5f3d2.

---
+++ Bus Seater /NotifsPermission.swift
@@ -0,0 +1,13 @@
+//
+//  NotifsPermission.swift
+//  Bus Seater
+//
+//  Created by Marvheen Theodore on 7/25/24.
+//
+
+import Foundation
+
+class NotifsPermission: ObservableObject{
+    @Published var WasPermissionAsked: Bool = false
+    @Published var WasPermissionGranted: Bool = false
+}
