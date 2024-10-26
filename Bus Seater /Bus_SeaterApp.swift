2ab9ca7384f14ceaf120a97a3236cce0e007eed2
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-10-25 20:02:58 -0400

    Update Bus_SeaterApp.swift

--- Bus Seater /Bus_SeaterApp.swift
+++ Bus Seater /Bus_SeaterApp.swift
@@ -23,7 +23,7 @@ struct YourApp: App {
   // register app delegate for Firebase setup
   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
   @StateObject private var lastUserInfo = LastUserInfo()
-  @StateObject private var notifsPermission = NotifsPermission()
+  @StateObject private var notifsPermissions = NotifsPermissions()
   var body: some Scene {
     WindowGroup {
       NavigationView {
