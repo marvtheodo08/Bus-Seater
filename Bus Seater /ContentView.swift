7c42038be208bc62cc2251c5e4551f11c6612670
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-11-01 08:00:42 -0400

    Update ContentView.swift
    Added Puesdocode code

--- Bus Seater /ContentView.swift
+++ Bus Seater /ContentView.swift
@@ -10,17 +10,21 @@ import Firebase
 import UserNotifications
 
 struct ContentView: View {
+    // set isSplash bool to true when app is opened
     @State private var isSplash = true
     @EnvironmentObject var lastUserInfo: LastUserInfo
     @EnvironmentObject var notifsPermissions: NotifsPermissions
     var body: some View{
         Group{
+            // if isSplash is true, show splash scree 
             if isSplash{
                 SplashScreen()
+                // make Splasb screen appear with an animation
                     .onAppear{
                         DispatchQueue.main.asyncAfter(deadline: .now() + 2){
                             withAnimation(.easeOut(duration: 0.5))
                             {
+                                // set splash to false when 
                                 isSplash = false
                             }
                             
@@ -28,11 +32,17 @@ struct ContentView: View {
                     }
                 
             }
+            //other wise if isSplash is false
             else{
+                  //make Login screen appear
                     Login()
+                    //When Login screen appears
                           .onAppear{
+                            //if NotifsPermission was not ask
                               if notifsPermissions.WasPermissionAsked == false{
+                                //Ask NotifsPermission
                                   requestNotificationPermission()
+                                //Change that Permission was asked
                                   notifsPermissions.changeAskedtoTrue()
                               }
                           }
