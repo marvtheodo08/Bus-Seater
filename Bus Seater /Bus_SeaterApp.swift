ef450a2a1e091dbe3b0f0c2294d66ba9627c9d86
marvtheodo08 <131079005+marvtheodo08@users.noreply.github.com>
2024-11-03 22:38:29 -0500

    Update Bus_SeaterApp.swift

--- Bus Seater /Bus_SeaterApp.swift
+++ Bus Seater /Bus_SeaterApp.swift
@@ -13,7 +13,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-      Auth.auth().useEmulator(withHost: "localHost", port: 9099)
+      Auth.auth().useEmulator(withHost: "localhost", port: 9099)
     return true
   }
 }
