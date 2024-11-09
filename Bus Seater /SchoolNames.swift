2d15514a0a630c96570e2bbcad637ea8310f79e0
Marvheen Theodore <marvheen.theodore08@gmail.com>
2024-11-09 10:28:21 -0500

    Added function to get names of schools of state

---
+++ Bus Seater /SchoolNames.swift
@@ -0,0 +1,29 @@
+//
+//  SchoolNames.swift
+//  Bus Seater 1
+//
+//  Created by Marvheen Theodore on 11/9/24.
+//
+
+import Foundation
+import Combine
+
+struct School: Codable {
+    let school_name: String
+}
+class GetSchools: ObservableObject {
+    @Published var schools: [School] = []
+    private var cancellables = Set<AnyCancellable>()
+    
+    func fetchSchools(state: String) {
+        guard let url = URL(string: "http://busseater-env.eba-nxi9tenj.us-east-2.elasticbeanstalk.com/\(state)") else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [School].self, decoder: JSONDecoder())
+            .replaceError(with: [])
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.schools, on: self)
+            .store(in: &cancellables)
+    }
+}
