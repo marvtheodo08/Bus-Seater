2aaba9543c98c36dd3f14fc5220587804f483823
Marvheen Theodore <marvheen.theodore08@gmail.com>
2024-12-19 17:42:38 -0500

    More indenting to login view code

--- Bus Seater /Login.swift
+++ Bus Seater /Login.swift
@@ -21,47 +21,45 @@ struct Login: View {
                 if userLoggingIn {
                     LoggingUserIn()
                 }
-                else {
-                    VStack {
-                        Text("Welcome to Bus Seater, the world's first bus seating app!")
-                            .multilineTextAlignment(.center)
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .foregroundStyle(.black)
-                        Spacer()
-                            .frame(height: 50)
-                        TextField("Email", text: $email)
-                            .keyboardType(.emailAddress)
-                            .textContentType(.emailAddress)
+                VStack {
+                    Text("Welcome to Bus Seater, the world's first bus seating app!")
+                        .multilineTextAlignment(.center)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(.black)
+                    Spacer()
+                        .frame(height: 50)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textContentType(.emailAddress)
+                        .padding()
+                        .background(Color.gray.opacity(0.3).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
+                        .accentColor(.black)
+                        .colorScheme(.light)
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.gray.opacity(0.3).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
+                        .accentColor(.black)
+                        .colorScheme(.light)
+                    Button(action: {login(email: email, password: password)}, label: {Text("Log in")
+                            .foregroundStyle(.white)
                             .padding()
-                            .background(Color.gray.opacity(0.3).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-                            .accentColor(.black)
-                            .colorScheme(.light)
-                        SecureField("Password", text: $password)
+                            .padding(.horizontal, 100)
+                            .background(Color.blue
+                                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
                             .padding()
-                            .background(Color.gray.opacity(0.3).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-                            .accentColor(.black)
-                            .colorScheme(.light)
-                        Button(action: {login(email: email, password: password)}, label: {Text("Log in")
-                                .foregroundStyle(.white)
-                                .padding()
-                                .padding(.horizontal, 100)
-                                .background(Color.blue
-                                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-                                .padding()
-                        })
-                        Button(action: {userSigningUp = true}, label: {Text("Don't have an account? Sign up here!")})
-                    }
-                    .padding()
-                    .navigationDestination(isPresented: $userSigningUp) {
-                        SignUp()
-                    }
-                    
+                    })
+                    Button(action: {userSigningUp = true}, label: {Text("Don't have an account? Sign up here!")})
+                }
+                .padding()
+                .navigationDestination(isPresented: $userSigningUp) {
+                    SignUp()
                 }
                 
             }
+            
         }
-        
     }
+    
     func login(email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
             if let error = error {
@@ -87,11 +85,6 @@ struct LoggingUserIn: View {
         }
     }
 }
-
-
-
-
-
 
 #Preview {
     Login()
