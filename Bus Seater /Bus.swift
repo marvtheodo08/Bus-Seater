ee59c94b16ad0fa18af2b5eb841e18d18646c1af
Marvheen Theodore <marvheen.theodore08@gmail.com>
2024-12-19 18:12:39 -0500

    Added additonal parameter to bus class

--- Bus Seater /Bus.swift
+++ Bus Seater /Bus.swift
@@ -10,11 +10,13 @@ import Combine
 
 struct Bus: Identifiable, Codable {
     let id: Int
+    let seatAmount: Int
     let busCode: String
     let schoolID: Int
     
     enum CodingKeys: String, CodingKey {
         case id
+        case seatAmount = "seat_amount"
         case busCode = "bus_code"
         case schoolID = "school_id"
     }
