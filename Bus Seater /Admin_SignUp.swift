b670dff9f073d1d2ceb0b10b28d846f64908faf6
Marvheen Theodore <marvheen.theodore08@gmail.com>
2025-04-14 18:48:47 -0400

    Added code to see why the obtainAccount function is not working as I expected

--- Bus Seater /Admin_SignUp.swift
+++ Bus Seater /Admin_SignUp.swift
@@ -30,6 +30,7 @@ struct Admin_SignUp: View {
     @State private var lastname: String = ""
     @State private var state: String = ""
     @State private var schoolID: Int = 0
+    @State var infoAppended: Bool = false
     @EnvironmentObject var newAccount: NewAccount
     @EnvironmentObject var lastUserInfo: LastUserInfo
     @EnvironmentObject var obtainAccountInfo: ObtainAccountInfo
@@ -90,13 +91,28 @@ struct Admin_SignUp: View {
                 .onAppear() {
                     Task {
                         try await newAccount.addAccount(NewAccount.Account(firstName: firstname, lastName: lastname, email: email, accountType: "admin", schoolID: schoolID))
-                        try await obtainAccountInfo.obtainAccountInfo(email: email)
+                        do {
+                            try await obtainAccountInfo.obtainAccountInfo(email: email)
+                        }
+                        catch {
+                            print("Failed to obtain Info: \(error)")
+                        }
+                        infoAppended = true
+                        if infoAppended {
+                            for account in obtainAccountInfo.account{
+                                lastUserInfo.Firstname = account.firstName
+                                lastUserInfo.Lastname = account.lastName
+                                lastUserInfo.schoolID = account.schoolID
+                                lastUserInfo.Email = account.email
+                                lastUserInfo.AccountType = account.accountType
+                                lastUserInfo.accountID = account.id
+                            }
+                            lastUserInfo.WasUserLoggedIn = true
+                        }
                     }
                 }
         }
     }
-    
-    
     
     func goBack() {
         switch currentStage {
