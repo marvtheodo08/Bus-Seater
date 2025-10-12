1cdaccd599a88913e3c822f4f65b8a8e3e5e6819
Marvheen Theodore <marvheen.theodore08@gmail.com>
2025-10-11 23:12:14 -0400

    Successfully formatted bus layout

--- Bus Seater /AssignStudent.swift
+++ Bus Seater /AssignStudent.swift
@@ -51,120 +51,107 @@ struct AssignStudent: View {
                         LazyVGrid(columns: columns, spacing: 16) {
                             // Seat formatting suggested by ChatGPT
                             ForEach(1...rowNum, id: \.self) { row in
-                                let columns = 5
-                                HStack {
-                                    ForEach(1...columns, id: \.self) {column in
-                                        if column < 3 && row < rowNum {
-                                            ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber < 3 }) { seat in
-                                                if seat.isOccupied {
-                                                    VStack{
-                                                        Text("This seat is taken")
-                                                            .font(.system(size: 3))
-                                                    }
-                                                    .foregroundStyle(.white)
-                                                    .frame(width: 15.0, height: 15.0)
-                                                    .padding(25)
-                                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                                                }
-                                                else {
-                                                    Button(action: {}, label: {
-                                                        VStack{
-                                                            Text("\(seat.rowNumber) \(seat.seatNumber)")
-                                                                .font(.system(size: 7))
-                                                        }
-                                                        .foregroundStyle(.white)
-                                                        .frame(width: 15.0, height: 15.0)
-                                                        .padding(25)
-                                                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                                                    })
-                                                }
-                                            }
-                                        }
-                                        else if column > 3 {
-                                            ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber > 2 }) { seat in
-                                                if seat.isOccupied {
-                                                    VStack{
-                                                        Text("This seat is taken")
-                                                            .font(.system(size: 3))
-                                                    }
-                                                    .foregroundStyle(.white)
-                                                    .frame(width: 15.0, height: 15.0)
-                                                    .padding(25)
-                                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                                                }
-                                                else {
-                                                    Button(action: {}, label: {
-                                                        VStack{
-                                                            Text("\(seat.rowNumber) \(seat.seatNumber)")
-                                                                .font(.system(size: 7))
-                                                        }
-                                                        .foregroundStyle(.white)
-                                                        .frame(width: 15.0, height: 15.0)
-                                                        .padding(25)
-                                                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                                                    })
-                                                }
-                                            }
-                                        }
-                                        else if column == 3 || row == rowNum && column == 2{
-                                            Color.clear.frame(height: 15.0)
-                                        }
-                                        else if row == rowNum && column > 3 {
-                                            ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber > 1 }) { seat in
-                                                if seat.isOccupied {
-                                                    VStack{
-                                                        Text("This seat is taken")
-                                                            .font(.system(size: 3))
-                                                    }
-                                                    .foregroundStyle(.white)
-                                                    .frame(width: 15.0, height: 15.0)
-                                                    .padding(25)
-                                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                                                }
-                                                else {
-                                                    Button(action: {}, label: {
-                                                        VStack{
-                                                            Text("\(seat.rowNumber) \(seat.seatNumber)")
-                                                                .font(.system(size: 7))
-                                                        }
-                                                        .foregroundStyle(.white)
-                                                        .frame(width: 15.0, height: 15.0)
-                                                        .padding(25)
-                                                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                                                    })
-                                                }
-                                            }
-                                        }
+                                if row < rowNum{
+                                    ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber <= 2 }) { seat in
+                                        SeatView(seat: seat)
+                                    }
+                                    
+                                    Color.clear
+                                        .frame(width: 15, height: 15)
+                                    
+                                    ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber > 2 }) { seat in
+                                        SeatView(seat: seat)
                                     }
                                 }
-                                
+                                else {
+                                    ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber == 1 }) { seat in
+                                        SeatView(seat: seat)
+                                    }
+                                    
+                                    Color.clear
+                                        .frame(width: 15, height: 15)
+                                    Color.clear
+                                        .frame(width: 15, height: 15)
+                                    
+                                    ForEach(seats.filter { $0.rowNumber == row && $0.seatNumber > 1 }) { seat in
+                                        SeatView(seat: seat)
+                                    }
+                                }
                             }
-                            .padding()
+                            
                         }
-                        .padding(.top, 50)
-                        
+                        .padding()
                     }
+                    .padding(.top, 50)
+                    
                 }
             }
         }
-    }
-    func obtainDriverInfo (accountID: Int) async throws -> Driver{
-        guard let url = URL(string: "https://bus-seater-hhd5bscugehkd8bf.canadacentral-01.azurewebsites.net/drivers/info/\(accountID)") else {
-            throw URLError(.badURL)
+        .onAppear {
+            Task {
+                driverInfo = try await obtainDriverInfo(accountID: UserDefaults.standard.integer(forKey: "accountID"))
+                busID = driverInfo?.busID ?? 0
+                busInfo = try await obtainBusInfo.obtainBusInfo(id: busID)
+                rowNum = busInfo?.rowAmount ?? 0
+                do {
+                    seats = try await getSeats.fetchSeats(busID: busID)
+                } catch {
+                    print("Failed to fetch seats: \(error)")
+                }
+                loadingSeats = false
+            }
         }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw URLError(.badServerResponse)
-        }
-        
-        print("Status code: \(httpResponse.statusCode)")
-        
-        let driverInfo = try JSONDecoder().decode(Driver.self, from: data)
-        return driverInfo
     }
 }
+
+struct SeatView: View {
+    let seat: Seat
+    var body: some View {
+        Group {
+            if seat.isOccupied {
+                VStack{
+                    Text("This seat is taken")
+                        .font(.system(size: 3))
+                }
+                .foregroundStyle(.white)
+                .frame(width: 15.0, height: 15.0)
+                .padding(25)
+                .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+            }
+            else {
+                Button(action: {}, label: {
+                    VStack{
+                        Text("\(seat.rowNumber) \(seat.seatNumber)")
+                            .font(.system(size: 7))
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: 15.0, height: 15.0)
+                    .padding(25)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                })
+
+            }
+        }
+    }
+}
+
+func obtainDriverInfo (accountID: Int) async throws -> Driver{
+    guard let url = URL(string: "https://bus-seater-hhd5bscugehkd8bf.canadacentral-01.azurewebsites.net/drivers/info/\(accountID)") else {
+        throw URLError(.badURL)
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        throw URLError(.badServerResponse)
+    }
+    
+    print("Status code: \(httpResponse.statusCode)")
+    
+    let driverInfo = try JSONDecoder().decode(Driver.self, from: data)
+    return driverInfo
+}
+
 
 #Preview {
     AssignStudent()
